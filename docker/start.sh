1417bf7188d09e35613366b241c9222047669fd0
Kevin-Duarte <kevinduarte2007@gmail.com>
2022-05-08 01:10:45 -0700

    Updated docker files

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-LD_RUN_PATH=/usr/local/lib sqlite-autoconf-3380200/configure --enable-optimizations
-export LD_LIBRARY_PATH="/usr/local/lib"
-git clone http://github.com/Kevin-Duarte/link-router
-python ./link-router/main.py
+git clone http://www.github.com/Kevin-Duarte/link-router /link-router-files/link-router
+python /link-router-files/link-router/main.py
